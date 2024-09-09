7e372e16ecf4e3cabc9550ec221f865a33051253
taeho5825 <69036689+taeho5825@users.noreply.github.com>
2024-09-09 12:13:37 +0900

    Update MainController.java

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -13,8 +13,11 @@ public class MainController {
         return "안녕하세요 sbb에 오신것을 환영합니다.";
     }
 
+    // root 메서드를 추가하고 / URL 매핑
     @GetMapping("/")
     public String root() {
+        // redirect:/question/list는 question/list URL로 페이지를 리다이렉트하라는 명령어
         return "redirect:/question/list";
     }
+    // localhost:8080로 접속하면 localhost:8080/question/list로 주소가 바뀌면서 질문 목록이 있는 웹 페이지로 연결
 }
