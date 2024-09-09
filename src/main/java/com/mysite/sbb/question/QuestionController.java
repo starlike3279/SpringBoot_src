fa620831e8922245727b18dca3eac01216f21bd3
taeho5825 <69036689+taeho5825@users.noreply.github.com>
2024-09-09 12:02:47 +0900

    Update QuestionController.java

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -10,14 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RequestMapping("/question")
-@RequiredArgsConstructor
+// @RequiredArgsConstructor :
+// questionRepository 객체를 주입
+// 롬복이 제공하는 애너테이션
+// final이 붙은 속성을 포함하는 생성자를 자동으로 만들어주는 역할을 함
+// QuestionController를 생성할 때 롬복으로 만들어진 생성자에 의해 questionRepository 객체가 자동으로 주입됨
+@RequiredArgsConstructor  
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model) {
+    //@ResponseBody 
+    public String list(Model model) { //매개변수로 Model을 지정하면 객체가 자동으로 생성된다.
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
